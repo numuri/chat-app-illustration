791450065032f96a3058d568b4953a4b729001b8
Nur Muhammad Rizky <nrmrizky5@gmail.com>
2023-07-10 22:26:38 +0700

    make css app illustration with frontend mentor

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        paleViolet: "hsl(276, 100%, 81%)",
+        moderateViolet: "hsl(276, 55%, 52%)",
+        desaturedDarkViolet: "hsl(271, 15%, 43%)",
+        grayishBlue: "hsl(206, 6%, 79%)",
+        veryDarkDesaturedViolet: "hsl(271, 36%, 24%)",
+        darkGrayishBlue: "hsl(270, 7%, 64%)",
+        lightMagenta: "hsl(293, 100%, 63%)",
+        lightViolet: "hsl(264, 100%, 61%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrayishViolet: "hsl(270, 20%, 96%)",
+        veryLightMagenta: "hsl(289, 100%, 72%)",
+      },
+    },
+  },
+  plugins: [],
+};
